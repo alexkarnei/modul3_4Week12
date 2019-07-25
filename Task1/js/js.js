1bd0b37ded186e135da6f65c0b4acca39aca48d2
AlexKarnei <46269001+AlexKarnei@users.noreply.github.com>
2019-07-25 13:32:33 +0300

    first commit

--- Task1/js/js.js
+++ Task1/js/js.js
@@ -1,9 +1,10 @@
 onload = function () {
-    for (var lnk = document.links, j = 0; j < lnk.length; j++)
-        if (lnk [j].href.includes("http://",0)) {
+    for (var lnk = document.links, j = 0; j < lnk.length; j++) {
+        if (lnk [j].innerHTML.includes("http://", 0)) {
             lnk [j].style.cssText = 'border-bottom:1px dotted  #000';
         } else {
             lnk [j].style.cssText = 'text-decoration: none';
         }
+    }
 }
 
