c983e8ef4af9a9f1c78ad4a300196fc4915b4717
AlexKarnei <46269001+AlexKarnei@users.noreply.github.com>
2019-07-25 13:18:17 +0300

    first commit

--- Task1/js/js.js
+++ Task1/js/js.js
@@ -1,9 +1,9 @@
-function check(a) {
-    if (a.valueOf().toString().indexOf("http://") === 0) {
-       a.style.textDecoration = "underline"
-    }
-    else {
-     a.style.backgroundColor = "black";
-    }
+onload = function () {
+    for (var lnk = document.links, j = 0; j < lnk.length; j++)
+        if (lnk [j].href.includes("http://",0)) {
+            lnk [j].style.cssText = 'border-bottom:1px dotted  #000';
+        } else {
+            lnk [j].style.cssText = 'text-decoration: none';
+        }
 }
 
